96fcfeaefc756fb1d8d2342f830c3fde23291d79
og <ovidio.garcia>
2024-06-19 15:42:50 +0200

    new function added

--- fortran/library/tame_library.F90
+++ fortran/library/tame_library.F90
@@ -18,7 +18,17 @@ use fabm_expressions
 
 public
 
-real(rk), PARAMETER ::  Pi = 3.1415927_rk
+real(rk), PARAMETER :: Pi = 3.1415927_rk
+real(rk), PARAMETER :: d_per_s = 1.0_rk/86400.0_rk
+real(rk), PARAMETER :: small_const = 1.0E-9_rk
 
+! colimitation gfunction
+elemental real(rk) function gfunc(x,y,n)
+    real(rk), intent(in) :: x,y,n
+    real(rk) :: r
+    r = (x+small_const)/(y+small_const)
+    r = r*(1.0_rk-r**n)/(1.0_rk-r**(n+1.0_rk))
+    gfunc = y*r*(1.0_rk+y*x/n+log(4.0_rk**(-1.0_rk/n) + 1.0_rk/(2.0_rk*n)))
+end function gfunc
 
 end module
