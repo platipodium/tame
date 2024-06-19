614bc7f9afaf5773b58cc617f1f56adb14988892
Carsten Lemmen <carsten.lemmen@hereon.de>
2024-06-19 15:53:11 +0200

    Added comments to tame_library.F90

--- fortran/library/tame_library.F90
+++ fortran/library/tame_library.F90
@@ -1,6 +1,7 @@
 !> @file tame_library.F90
 !> @brief tame_library module
 ! SPDX-FileContributor Carsten Lemmen <carsten.lemmen@hereon.de
+! SPDX-FileContributor Ovidio
 
 #include "fabm_driver.h"
 
@@ -23,9 +24,19 @@ real(rk), PARAMETER :: d_per_s = 1.0_rk/86400.0_rk
 real(rk), PARAMETER :: small_const = 1.0E-9_rk
 
 ! colimitation gfunction
+! Written for 2 inputs, if you apply it successively, it could
+! deal with n inputs
+! @todo there should be a function gfunc_infinite that calls this one.
+! It is a surrogate function developed by Kai for the queuing function
+!@param x  a limiting quantity (or aggregate of some limiting quantity)
+!@param y  another limiting quantity (or aggregate of some limiting quantity)
+!@param n  shape parameter
 elemental real(rk) function gfunc(x,y,n)
+
+    implicit none
     real(rk), intent(in) :: x,y,n
     real(rk) :: r
+
     r = (x+small_const)/(y+small_const)
     r = r*(1.0_rk-r**n)/(1.0_rk-r**(n+1.0_rk))
     gfunc = y*r*(1.0_rk+y*x/n+log(4.0_rk**(-1.0_rk/n) + 1.0_rk/(2.0_rk*n)))
