a0d9a52645743ec998a2a0f9da6cbd7cb3ec97af
Carsten Lemmen <carsten.lemmen@hereon.de>
2025-02-19 18:49:17 +0100

    Added type_group

--- fortran/chemistry/chemistry.F90
+++ fortran/chemistry/chemistry.F90
@@ -8,13 +8,15 @@
 module chemistry
 
    use fabm_types
-   use chemistry_types, only : global_molecule_table
+   use chemistry_types, only : global_molecule_table, type_group
+   use iso_fortran_env, only : stdout => output_unit, stderr => error_unit
    implicit none
    private
 
    type, extends(type_base_model), public :: type_chlorophyll
 
       type(type_state_variable_id) :: id_chlorophyll
+      type(type_group) :: din, nutrient
 
    contains
       procedure :: initialize
@@ -26,17 +28,27 @@ subroutine initialize(self,configunit)
 
    class (type_chlorophyll), intent(inout), target :: self
    integer,            intent(in)            :: configunit
-   
+
+   character(len=3) :: din_molecules(3) = (/'NO3','NH4','NO2'/)
+
    call global_molecule_table%register('Methane','CH4')
    call global_molecule_table%register('Chlorophyll-a','C55H72MgN4O5')
+   call global_molecule_table%register('Nitrate','NO3')
+   call global_molecule_table%register('Nitrite','NO2')
+   call global_molecule_table%register('Ammonium','NH4')
+   call global_molecule_table%register('Phosphate','PO4')
    
+   call self%din%create('DIN',din_molecules)
+   call self%nutrient%create('Nutrient',(/'NO3','NH4','NO2','PO4'/))
+
    !C55H72MgN4O5
    !Molar mass	893.509 g·mol−1
 
    ! Register state variables
    call self%register_state_variable(self%id_chlorophyll,'chl','', '')
 
+   !write(stderr, *) global_molecule_table%get('Chlorophyll-a')
 
-   end subroutine initialize
+end subroutine initialize
 
 end module chemistry
