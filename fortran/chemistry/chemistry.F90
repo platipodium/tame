abb6dba747df0d4006418433d1c5edcc8f5e1177
Carsten Lemmen <carsten.lemmen@hereon.de>
2025-02-20 19:54:56 +0100

    Started implementing more BGC

--- fortran/chemistry/chemistry.F90
+++ fortran/chemistry/chemistry.F90
@@ -15,11 +15,12 @@ module chemistry
 
    type, extends(type_base_model), public :: type_chlorophyll
 
-      type(type_state_variable_id) :: id_chlorophyll
-      type(type_group) :: din, nutrient
+      type(type_state_variable_id), allocatable :: id_var(:)
+      type(type_group), allocatable :: state_variables(:)
 
    contains
       procedure :: initialize
+      procedure :: do
    end type
 
 contains
@@ -29,24 +30,76 @@ subroutine initialize(self,configunit)
    class (type_chlorophyll), intent(inout), target :: self
    integer,            intent(in)            :: configunit
 
+   integer :: i, n
+
    call global_molecule_table%register('Methane','CH4')
    call global_molecule_table%register('Chlorophyll-a','C55H72MgN4O5')
    call global_molecule_table%register('Nitrate','NO3')
    call global_molecule_table%register('Nitrite','NO2')
    call global_molecule_table%register('Ammonium','NH4')
    call global_molecule_table%register('Phosphate','PO4')
-   
-   call self%din%create('DIN',(/'NO3','NH4','NO2'/))
-   call self%nutrient%create('Nutrient',(/'NO3','NH4','NO2','PO4'/))
 
-   !C55H72MgN4O5
-   !Molar mass	893.509 g·mol−1
+   ! Define here the (number of) groups that you would like to
+   ! integrate as interior state variables.  Groups are
+   ! single molecules or molecule
+   ! groups consisting of molecules registered in the global_molecule_table
+   allocate(self%state_variables(2))
+   call self%state_variables(1)%create('DIN',(/'NO3','NH4','NO2'/))
+   call self%state_variables(2)%create('PO4',(/'PO4'/))
 
-   ! Register state variables
-   call self%register_state_variable(self%id_chlorophyll,'chl','', '')
+   ! Define here the list of elements that should be considered as state
+   ! variables
+   !allocate(elements(3))
+   !elements = (/'C','N','P'/)
 
-   !write(stderr, *) global_molecule_table%get('Chlorophyll-a')
+   n = ubound(self%state_variables,1)
+
+   allocate(self%id_var(n))
+   do i = 1,n
+      call self%register_state_variable(self%id_var(i), &
+        self%state_variables(i)%name,'dummy unit','dummy long name')
+   enddo
 
 end subroutine initialize
+
+subroutine do(self, _ARGUMENTS_DO_)
+   _DECLARE_ARGUMENTS_DO_
+   class (type_chlorophyll), intent(in) :: self
+
+   integer       :: i, n
+   real(kind=rk), allocatable :: rhs(:)
+   real(kind=rk), allocatable :: state_variables(:)
+
+   n = ubound(self%id_var,1)
+   if (n<1) return
+
+   allocate(rhs(n))
+   allocate(state_variables(n))
+
+   _LOOP_BEGIN_
+
+      ! Get state variables into local variable
+      do i = 1, n
+         _GET_(self%id_var(i), state_variables(i))
+      enddo
+
+      !do i = 1, num_elements ! e.g., N  ( C, Si, Fe, P)
+      !_GET_(self%id_var(det_index(i)), det_element(i))  ! Detritus Organics in mmol-C/m**3
+      !_GET_(self%id_var(dom_index(i)), dom_element(i))  ! Dissolved Organics in mmol-C/m**3
+      !end do
+
+   ! here, nutrients are only remineralised (e.g., uptake in tame_phy)
+   !do i = 1,num_chemicals
+   !   rhs(dix_index(i)) = remin_chemical(i) !+ nut_prod(i)
+   !end do
+
+   ! Communicate right hand sides to FABM
+   do i = 1, n
+      _ADD_SOURCE_(self%id_var(i), rhs(i))
+   end do
+
+   deallocate(rhs)
+   _LOOP_END_
+end subroutine do
 
 end module chemistry
