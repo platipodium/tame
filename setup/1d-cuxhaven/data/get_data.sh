c6c36dff912d113865fe91371de2535160b99688
Carsten Lemmen <carsten.lemmen@hereon.de>
2025-05-24 18:41:13 +0200

    Consulted shellcheck for improving bash files

--- setup/1d-cuxhaven/data/get_data.sh
+++ setup/1d-cuxhaven/data/get_data.sh
@@ -1,22 +1,21 @@
+#! /bin/bash
 # SPDX-FileCopyrightText: 2025 Helmholtz-Zentrum hereon GmbH
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileContributor Ovidio Garcia-Oliva <ovidio.garcia@hereon.de>
 
-NAMES=( "Cuxhaven_DWD!Lufttemperatur" 
-        "Cuxhaven_DWD!Windgeschwindigkeit" 
+NAMES=( "Cuxhaven_DWD!Lufttemperatur"
+        "Cuxhaven_DWD!Windgeschwindigkeit"
         "Cuxhaven_DWD!Windrichtung"
-)  
+)
 
 for NAME in "${NAMES[@]}"; do
     FILENAME="$NAME.zip"
+    test -f "${FILENAME}" && continue
     URL="https://www.kuestendaten.de/DE/dynamisch/appl/data/daten_prod/prodNeuProd/direct_download/$FILENAME"
-    wget $URL -O $FILENAME
-    unzip $FILENAME
+    wget "${URL}" -O "${FILENAME}"
+    unzip "${FILENAME}"
     TXTFILE="${NAME}.txt"
-    sed -i'' '/^[^0-9]/ s/^/# /' "$TXTFILE" #Comment lines that are not numerical
+    sed -i'' '/^[^0-9]/ s/^/# /' "${TXTFILE}" #Comment lines that are not numerical
 done
 
-rm *.zip
-rm *.txt
-
-Rscript setup_data.R
+Rscript setup_data.R && (rm -f ./*.zip ./*.txt)
