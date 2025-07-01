e18e8030108daf208eb3fee4c8cc532b8d424587
Thomas Imbert <thomas.imbert@hereon.de>
2025-07-01 12:00:41 +0200

    Zoo stoich, needs further check

--- tame/zoo.F90
+++ tame/zoo.F90
@@ -18,16 +18,17 @@ module tame_zooplankton
 
    private
    type, extends(type_base_model), public :: type_tame_zooplankton
+
       !! State variables ids
-      type (type_state_variable_id) :: id_biomass ! biomass
+      type (type_state_variable_id) :: id_zooplankton_C ! zooplankton_C
 
       !! Dependency ids
-      type (type_dependency_id) :: id_prey_Q(NUM_ELEM) ! Prey stoichiometry
+      type (type_dependency_id) :: id_prey_Q(NUM_ELEM) ! Prey and stoichiometry
       type (type_state_variable_id) :: id_prey, id_dom_(NUM_ELEM), id_det_(NUM_ELEM) ! prey
 
       !! Diagnostic variable ids
-      !type (type_diagnostic_variable_id) :: id_nut, id_nut2, id_rate
-      type (type_diagnostic_variable_id) :: id_dummy, id_dummy_sloppy
+      type (type_diagnostic_variable_id) :: id_dummye, id_dummya, id_dummy_N, id_dummy_P
+      type (type_diagnostic_variable_id) :: id_zoo_elem(NUM_ELEM) ! Zooplankton stoichiometry
 
       !! Model parameters
       real(rk) :: max_ingestion, saturation, sloppy ! maximum ingestion_rate rate, food saturation
@@ -58,7 +59,7 @@ module tame_zooplankton
       call self%get_parameter(self%sloppy, 'sloppy', '', 'sloppy feeding', default=0._rk)
 
       !! Register state variables
-      call self%register_state_variable(self%id_biomass,'biomass', 'mmol-C m-3', 'concentration', 1.0_rk, minimum=0.0_rk)
+      call self%register_state_variable(self%id_zooplankton_C,'zooplankton_C', 'mmol-C m-3', 'concentration', 1.0_rk, minimum=0.0_rk)
 
       !! Register environmental dependencies
 
@@ -66,10 +67,16 @@ module tame_zooplankton
       call self%register_state_dependency(self%id_prey, 'prey','mmol-C m-3', 'prey source')
       do i = 1,NUM_ELEM
          elem = ElementList(i:i)
+                     
+         call self%register_state_dependency(self%id_det_(i), 'det_' // elem,'mol-' // elem // ' mol-C-1', 'Particulate Organic' // elem)
+
          if (elem .NE. 'C') then
             call self%register_state_dependency(self%id_dom_(i), 'dom_' // elem,'mol-' // elem // ' mol-C-1', 'Dissolved Organic' // elem)
 
-            call self%register_state_dependency(self%id_det_(i), 'det_' // elem,'mol-' // elem // ' mol-C-1', 'Particulate Organic' // elem)
+            ! Element stoichiometry in Zooplankton
+            call self%register_diagnostic_variable(self%id_zoo_elem(i), 'zoo_' // elem,'mol-' // elem // ' m-3', 'zooplankton ' // elem)
+            call self%add_to_aggregate_variable(type_universal_standard_variable(name='total_' // trim(ElementName(i)), units='mmol-' // elem // ' m-3', &
+              aggregate_variable=.true., conserved=.true.), self%id_zoo_elem(i))
 
       !! Retrieve prey stoichiometric composition (if FlexStoich)
             call self%register_dependency(self%id_prey_Q(i), 'PreyQ_' // elem,'mol-' // elem // ' mol-C-1', elem // ':C-quota')
@@ -77,28 +84,29 @@ module tame_zooplankton
          endif
       end do ! Prey is supposed to be in carbon, so only extracting other elements, if available
 
-      call self%register_diagnostic_variable(self%id_dummy, 'dummy','', '')
-      call self%register_diagnostic_variable(self%id_dummy_sloppy, 'dummy_sloppy','', '')
+      call self%register_diagnostic_variable(self%id_dummye, 'dummye','', '')
+      call self%register_diagnostic_variable(self%id_dummya, 'dummya','', '')
+      
+      call self%register_diagnostic_variable(self%id_dummy_N, 'dummy_N','', '')
+      call self%register_diagnostic_variable(self%id_dummy_P, 'dummy_P','', '')
 
-      ! call self%register_diagnostic_variable(self%id_nut, 'nut1','mmol-? m-3', 'nutrient related')
-      ! call self%register_diagnostic_variable(self%id_nut2,'nut2','mmol-? m-3', 'nutrient related')
-      ! call self%register_diagnostic_variable(self%id_rate,'rate','mmol-? m-3', 'nutrient related')
    end subroutine initialize
 
-   subroutine do(self, _ARGUMENTS_DO_)
+   subroutine do(self, _ARGUMENTS_DO_) 
       class (type_tame_zooplankton), intent(in) :: self
       _DECLARE_ARGUMENTS_DO_
       integer :: i ! dummy index
-      real(rk) :: biomass, prey, ivlev
-      real(rk) :: ingestion_rate, respiration_rate, new, growth_rate, assimilation_rate
+      real(rk) :: zooplankton_C, prey, ivlev, C_consumption
+      real(rk) :: ingestion_rate, respiration_rate, new, growth_rate
       real(rk) :: exudation_rate(NUM_ELEM), elem_Q(NUM_ELEM), Q_diff(NUM_ELEM) ! elem_Q is the prey quotas
       real(rk) :: C_excess(NUM_ELEM), elem_assimilation(NUM_ELEM)              ! assimilation or excess of nutrients
+      real(rk) :: zoo_stoich_scale!(NUM_ELEM)                                   ! adjust the zooplankton stoichiometry when respiration > 0
       real(rk) :: sloppy_feeding(NUM_ELEM), excretion_rate(NUM_ELEM)
       character :: elem
 
       ! Enter spatial loops (if any)
       _LOOP_BEGIN_
-      _GET_(self%id_biomass, biomass) ! biomass carbon
+      _GET_(self%id_zooplankton_C, zooplankton_C) ! zooplankton_C carbon
 
       !@what: is zooplankton exudating to elem_Q pool or to DOM pool?
       do i = 1, NUM_ELEM
@@ -115,7 +123,6 @@ module tame_zooplankton
       !! Predation
       ivlev = 1.0_rk - exp(- prey / self%saturation )
       ingestion_rate = self%max_ingestion * ivlev
-      ! _SET_DIAGNOSTIC_(self%id_rate, ingestion_rate )
 
       !! Losses
       respiration_rate = self%resp !@todo: include other physiological loss terms here
@@ -125,7 +132,7 @@ module tame_zooplankton
       !do i = 1,NUM_ELEM
       !   elem = ElementList(i:i)
       !   if (elem .NE. 'C') then
-            !exudation_rate(i) = max(0.0_rk, elem_Q(i) -  zoo_fixed_stoichiometry(i) ) * ingestion_rate*biomass * self%sloppy  !
+            !exudation_rate(i) = max(0.0_rk, elem_Q(i) -  zoo_fixed_stoichiometry(i) ) * ingestion_rate*zooplankton_C * self%sloppy  !
             ! Should exudate C if elem is limiting?
 
             ! Difference in stoichiometry of the prey, related to the copepods'
@@ -139,27 +146,48 @@ module tame_zooplankton
       !      elem_diff_to_limiting = Q_diff - min( Q_diff )
       !      elem_excess = elem_diff_to_limiting * prey * elem_Q ! Proportion of elements in excess, in relation to the most limiting
 
-            !sloppy_feeding(i) = ingestion_rate*biomass * (1 - self%sloppy) * elem_Q(i)
+            !sloppy_feeding(i) = ingestion_rate*zooplankton_C * (1 - self%sloppy) * elem_Q(i)
       !   endif
       !end do
 
+      ! Correction term for elements quota difference (prey / zoo) when respiration_rate > 0
+      !zoo_stoich_scale = zooplankton_C * zoo_fixed_stoichiometry / (zooplankton_C - respiration_rate) / zoo_fixed_stoichiometry
+
       ! Resolving the stoechiometry in zooplankton
+      C_consumption = respiration_rate / ingestion_rate
+
       do i = 1,NUM_ELEM
          elem = ElementList(i:i)
          if (elem .NE. 'C') then
 
             ! Relative difference in the stoichiometry of the prey
-            Q_diff(i) = (elem_Q(i) -  zoo_fixed_stoichiometry(i) + respiration_rate) / zoo_fixed_stoichiometry(i)
+            !Q_diff(i) = (elem_Q(i) -  zoo_fixed_stoichiometry(i) / ( 1._rk + respiration_rate ) ) / zoo_fixed_stoichiometry(i)
+            
+            !Q_diff(i) = (elem_Q(i) -  zoo_fixed_stoichiometry(i)) / zoo_fixed_stoichiometry(i) + respiration_rate / ingestion_rate
 
-            if (Q_diff(i) < 0 ) then ! Element is limiting
-               C_excess(i) = -Q_diff(i) ! C excess in the prey, in %
-               elem_assimilation(i) = 1._rk ! Element assimilation, in %
+            !zoo_stoich_scale = zoo_fixed_stoichiometry(i) / (1._rk + respiration_rate / ingestion_rate)
+            !Q_diff(i) = (elem_Q(i) -  zoo_stoich_scale) / zoo_stoich_scale !+ respiration_rate / ingestion_rate
+            
+            if ( C_consumption .GE. 1 ) then ! Simplification if respiration exceeds grazing
+               C_excess(i) = 0._rk
+               elem_assimilation(i) = 0._rk ! 0% assimilation as all the C ingested is used for maintenance
 
-            else ! The element is in excess
-               C_excess(i) = 0._rk                             ! Only a part of the element is excreeted to be in balamce with C
-               elem_assimilation(i) = 1._rk / ( Q_diff(i) + 1._rk ) ! % of element assimilated, the rest is excreeted
+            else ! Simplification if respiration is not exceeding
 
-            endif
+               zoo_stoich_scale = elem_Q(i) / (1._rk - respiration_rate / ingestion_rate) ! R / I of the C will be consumed for maintenance costs
+               Q_diff(i) = (zoo_stoich_scale - zoo_fixed_stoichiometry(i)) / zoo_fixed_stoichiometry(i) !+ respiration_rate / ingestion_rate
+
+               if (Q_diff(i) < 0 ) then        ! Element is limiting
+                  C_excess(i) = -Q_diff(i)     ! C excess in the prey, in %
+                  elem_assimilation(i) = 1._rk ! Element assimilation, in %
+
+               else ! The element is in excess
+                  C_excess(i) = 0._rk                                  ! Only a part of the element is excreted to be in balance with C
+                  elem_assimilation(i) = 1._rk / ( Q_diff(i) + 1._rk ) ! % of element assimilated, the rest is excreted
+
+               endif
+
+            endif 
 
          endif
       end do
@@ -171,41 +199,46 @@ module tame_zooplankton
       ! Growth rate of the copepod
       growth_rate = ingestion_rate * ( 1._rk - maxval(C_excess) ) ! Carbon that is not excreted is used
 
-      _ADD_SOURCE_(self%id_prey, -ingestion_rate*biomass * days_per_sec ) ! Prey is ingested
-      _ADD_SOURCE_(self%id_biomass, (growth_rate - respiration_rate) * biomass * days_per_sec ) ! Zooplankton growth
+      _ADD_SOURCE_(self%id_prey, -ingestion_rate * zooplankton_C * days_per_sec ) ! Prey is ingested
+      _ADD_SOURCE_(self%id_zooplankton_C, (growth_rate - respiration_rate) * zooplankton_C * days_per_sec ) ! Zooplankton growth
 
       ! Excretion and ingestion of elements
-      excretion_rate = 1._rk - assimilation_rate  ! What is not assimilated is excreted
-      excretion_rate(1) = maxval(C_excess)       ! Carbon excretion
+      excretion_rate = 1._rk - elem_assimilation  ! What is not assimilated is excreted
+      excretion_rate(1) = maxval(C_excess)        ! Carbon excretion
 
-      !! Zooplankton can stor C in fat, should not excrete it directly, maybe as a state variable?
-      ! As an state variable, controlled by a boolean (IF_STORAGE T/F)
+      !! Zooplankton can store C in fat, should not excrete it directly, maybe as a state variable?
+      ! As a state variable, controlled by a boolean (IF_STORAGE T/F)
 
       ! Resolving the DOM and POM targets with biogeochemistry
-      _SET_DIAGNOSTIC_(self%id_dummy, exudation_rate(2))
-      _SET_DIAGNOSTIC_(self%id_dummy_sloppy, sloppy_feeding(2))
-
+      
       do i = 1, NUM_ELEM
          elem = ElementList(i:i)
-         if (elem .NE. 'C') then
-            _ADD_SOURCE_( self%id_dom_(i), excretion_rate(i) * ingestion_rate * prey * elem_Q(i) ) !* days_per_sec DOM target for later
-            _ADD_SOURCE_( self%id_det_(i), sloppy_feeding(i) ) ! POM target for later
 
-         !else
-         !   _ADD_SOURCE_( self%id_dom_(i), ( excretion_rate(i) * ingestion_rate + respiration_rate ) * prey  ) ! DOM target for later
-         endif                                                                   ! DIC
+         if (growth_rate - respiration_rate < 0) then ! Zooplankton starves and dies
+               _ADD_SOURCE_( self%id_det_(i), -(growth_rate - respiration_rate) * zooplankton_C * zoo_fixed_stoichiometry(i) * days_per_sec ) ! Fecal pellets: POC 
+         endif
+
+         if (elem .NE. 'C') then
+            _ADD_SOURCE_( self%id_dom_(i), excretion_rate(i) * zooplankton_C * ingestion_rate * elem_Q(i) * days_per_sec ) ! DOM target for later
+            
+            !_ADD_SOURCE_( self%id_det_(i), sloppy_feeding(i) ) ! POM target for later
+            _SET_DIAGNOSTIC_(self%id_zoo_elem(i), zoo_fixed_stoichiometry(i) * zooplankton_C ) 
+
+         else
+            _ADD_SOURCE_( self%id_det_(i), ingestion_rate * maxval(C_excess) * zooplankton_C * days_per_sec ) ! Fecal pellets: POC 
+         endif                                                             ! DIC ?
+
       end do
 
-      !! exudation_rate to DOM (proportional to C-respiration_rate)
-      !new = respiration_rate * biomass
-      !do i = 1,NUM_ELEM
-         !if (ElementList(i:i) .NE. 'C') _ADD_SOURCE_(self%id_var(dom_index(i)), new*stoichiometry(i) UNIT) ! elem_Qs sink
-      !end do
-      !_SET_DIAGNOSTIC_(self%id_nut, new*stoichiometry(2) )
-      !_SET_DIAGNOSTIC_(self%id_nut2, new*stoichiometry(3) )
+
+      _SET_DIAGNOSTIC_(self%id_dummya, 1 - maxval(C_excess) )! maxval(C_excess) )  
+      _SET_DIAGNOSTIC_(self%id_dummye, maxval(elem_assimilation) )
+      !_SET_DIAGNOSTIC_(self%id_dummy_N, ingestion_rate * elem_Q(2) * elem_assimilation(2) / ( (growth_rate - respiration_rate) * zoo_fixed_stoichiometry(2) ) )
+      _SET_DIAGNOSTIC_(self%id_dummy_N, respiration_rate )
+      _SET_DIAGNOSTIC_(self%id_dummy_P, ingestion_rate * elem_Q(3) * elem_assimilation(3) / ( (growth_rate - respiration_rate) * zoo_fixed_stoichiometry(3) ) )
 
       ! sinking to POM
-      !new = sinking * biomass
+      !new = sinking * zooplankton_C
 
       !! @what: should not be sinking be managed by physical driver? I see this useful probably only on 0d setups. A flag is required!
       !do i = 1,NUM_ELEM ! C, N, P (Si, Fe)
