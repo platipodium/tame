7e3dcbfa524c3b821b72ea065dd794b46ad18dea
Kai Wirtz <kai.wirtz@hereon.de>
2025-10-05 09:50:28 +0200

    zoo+phy merged to life

--- tame/tame_model_library.F90
+++ tame/tame_model_library.F90
@@ -23,13 +23,14 @@ contains
 
    ! Add new tame modules here
    use tame_bgc
+   use tame_life
    use chemistry
    !use tame_chemistry_phytoplankton
    use tame_chemistry_detritus
    use tame_chemistry_nutrient
    use tame_chemistry_sink
-   use tame_phyto
-   use tame_zooplankton
+   !use tame_phyto
+   ! use tame_zooplankton
    !use tame_temperature
    use tame_time
 
@@ -49,9 +50,10 @@ contains
       case ('chemistry_sink');   allocate(type_tame_chemistry_sink::model)
          !case ('chlorophyll');   allocate(type_chlorophyll::model)
 !         case ('phytoplankton');   allocate(type_tame_phytoplankton::model)
-         case ('phyto');   allocate(type_tame_phyto::model)
+!         case ('phyto');   allocate(type_tame_phyto::model)
+         case ('life');   allocate(type_tame_life::model)
          case ('time');   allocate(type_tame_time::model)
-         case ('zooplankton');   allocate(type_tame_zooplankton::model)
+!         case ('zooplankton');   allocate(type_tame_zooplankton::model)
 !         case ('temperature');   allocate(type_tame_temperature::model)
 
          ! Add new tame models here
